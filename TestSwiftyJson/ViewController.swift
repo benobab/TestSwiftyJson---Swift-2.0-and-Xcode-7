26d7550bfa60759cb1563bcda117431cb6c7d4ba
Benjamin Lacroix <benlacroix83@gmail.com>
2015-07-30 22:53:41 +0100

    Initial Commit

--- TestSwiftyJson/ViewController.swift
+++ TestSwiftyJson/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
-
+import SwiftyJSON
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let file = NSBundle(forClass:AppDelegate.self).pathForResource("SwiftyJSONTests", ofType: "json") {
+            let data = NSData(contentsOfFile: file)!
+            let json = JSON(data:data)
+            let creationDate = json[0]
+            let date = creationDate["created_at"]
+            print(date)
+        }
     }
 
     override func didReceiveMemoryWarning() {
